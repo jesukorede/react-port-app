45133677fc77e48f4946acd5eeface507f271305
jesukorede <benjaminjesukorede@gmail.com>
2023-12-06 15:55:09 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter basename="/react-port-app">
   <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
