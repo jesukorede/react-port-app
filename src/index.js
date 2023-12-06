c6f02978debf550e3f78706965921ee3a309e46a
jesukorede <benjaminjesukorede@gmail.com>
2023-12-06 16:12:27 +0100

    intresting

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom"
 
-ReactDOM.render(
-  <HashRouter basename="/react-port-app">
-  <App />
-  </HashRouter>,
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(
+  <React.StrictMode>
+    <HashRouter>
+     <App />
+    </HashRouter>
+  </React.StrictMode>
+  );
+
+  
+// ReactDOM.render(
+//   <HashRouter basename="/react-port-app">
+//   <App />
+//   </HashRouter>,
+//   document.getElementById("root")
+// );
